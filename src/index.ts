63db9bcc51ec7f54f907fe0d39e0c8b144628ebc
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-26 03:46:50 -0800

    listener

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import { token } from "./config";
 import { Client as Irc } from "irc";
 
 const discord = new Discord({ intents: ["Guilds", "GuildMessages"] });
-const irc = new Irc("irc.libera.chat", "pipojs", { port: 6697 });
+const irc = new Irc("irc.libera.chat", "pipojs", {
+  port: 6697,
+  channels: ["#redbook"],
+});
+
+irc.addListener("message", function (from, to, message) {
+  console.log(from + " => " + to + ": " + message);
+});
 
 discord.once("ready", () => {
   console.log("ready");
@@ -13,4 +20,4 @@ discord.on("messageCreate", (m) => {
   console.log(m);
 });
 
-discord.login(token).then((res) => console.log(res));
+discord.login(token).then(() => console.log("discord: logged in"));
