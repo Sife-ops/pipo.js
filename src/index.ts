a8c7007a06d598f2c5a9c782561abd3b930f963b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-26 02:49:45 -0800

    discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello');
+import { Client } from "discord.js";
+import { token } from "./config";
+
+const client = new Client({ intents: ["Guilds"] });
+
+client.once("ready", () => {
+  console.log("ready");
+});
+
+client.on("messageCreate", (m) => {
+  console.log(m);
+});
+
+client.login(token).then((res) => console.log(res));
