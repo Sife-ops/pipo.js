9a027b662500573b8d900b3018a02e78257a0156
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-26 04:16:15 -0800

    geekshed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Client as Discord } from "discord.js";
-import { token } from "./config";
 import { Client as Irc } from "irc";
+import { token } from "./config";
 
 const discord = new Discord({ intents: ["Guilds", "GuildMessages"] });
-const irc = new Irc("irc.libera.chat", "pipojs", {
+
+// todo: liberachat
+const irc = new Irc("irc.geekshed.net", "pipojs", {
   port: 6697,
   channels: ["#redbook"],
+  secure: true,
 });
 
 irc.addListener("message", function (from, to, message) {
