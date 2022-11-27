8d7c60accfd745ad973325c4b8e3d4a5ccc122a4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-26 19:45:56 -0800

    move to config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { Client as Discord, TextChannel } from "discord.js";
 import { Client as Irc } from "irc";
-import { token } from "./config";
+import * as config from "./config";
 
 const discord = new Discord({
   intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
 // todo: liberachat
-const irc = new Irc("irc.geekshed.net", "pipojs", {
-  port: 6697,
-  channels: ["#redbook"],
+const irc = new Irc(config.irc.server, config.irc.nick, {
+  port: config.irc.port,
+  channels: [config.irc.channel],
   secure: true,
   debug: true,
 });
@@ -23,14 +23,15 @@ discord.once("ready", () => {
 });
 
 discord.on("messageCreate", (m) => {
-  if (m.author.id === "1046005694769414174") {
-    return;
+  if (m.author.id !== config.discord.application_id) {
+    irc.say(config.irc.channel, `${m.author.username} (Discord): ${m.content}`);
   }
-  irc.say("#redbook", `${m.author.username} (Discord): ${m.content}`);
 });
 
 irc.addListener("message", function (from, to, message) {
-  const ts = discord.channels.cache.get("990776652604801044") as TextChannel;
+  const ts = discord.channels.cache.get(
+    config.discord.channel_id
+  ) as TextChannel;
   ts.send(`${from} (IRC): ${message}`);
 });
 
@@ -38,4 +39,6 @@ irc.addListener("message", function (from, to, message) {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-discord.login(token).then(() => console.log("discord: logged in"));
+discord
+  .login(config.discord.token)
+  .then(() => console.log("discord: logged in"));
